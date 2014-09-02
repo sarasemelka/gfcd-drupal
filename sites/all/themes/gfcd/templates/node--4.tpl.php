af7cd98e4b0a80af1cf2c2306834929831cd670f
Brad Sawicki <git@bradsawicki.com>
2014-09-02 13:09:16 -0500

    Comment Our Sub Nav/Adv Toolkit

--- sites/all/themes/gfcd/templates/node--4.tpl.php
+++ sites/all/themes/gfcd/templates/node--4.tpl.php
@@ -7,10 +7,12 @@
   </div>
 </section>
 
+<!--
 <nav class="sub-nav">
   <a href="#main">Organize Your Dance</a>
   <a href="#toolkit">Advocacy Toolkit</a>
 </nav>
+-->
 
 <section id="main" class="main">
   <div class="wrapper">
@@ -83,19 +85,21 @@
 </section>
 
 <?php
-  $nid = 9;
-  $node2 = node_load($nid);
-  node_view($node2);
+  // $nid = 9;
+  // $node2 = node_load($nid);
+  // node_view($node2);
 ?>
+<!--
 <section id="toolkit" class="toolkit">
   <div class="wrapper">
 
   <div class="body">
-    <h2><?php print $node2->title; ?></h2>
-    <?php print $node2->body['und'][0]['value']; ?>
+    <h2><?php // print $node2->title; ?></h2>
+    <?php // print $node2->body['und'][0]['value']; ?>
   </div>
   
   </div>
 </section>
+-->
 
 <?php // dpm($node2); ?> 
