9d1c61ebdd72720c1ff697f8cea61e4814387c3c
Brad Sawicki <git@bradsawicki.com>
2014-09-02 15:27:41 -0500

    Home Song Download block

--- sites/all/themes/gfcd/templates/node--3.tpl.php
+++ sites/all/themes/gfcd/templates/node--3.tpl.php
@@ -6,6 +6,15 @@
   </div>
 </section>
 
+<section class="song-download">
+  <div class="wrapper">
+    <?php
+    $block = module_invoke('block', 'block_view', '15');
+    print render($block['content']);
+    ?>
+  </div>
+</section>
+
 <section id="main" class="main">
   <div class="wrapper">
          
@@ -42,46 +51,26 @@
 
 <section class="bottom">
     
-    <div class="col-1-2 block-get-involved">
-      <h3 class="block-title">Get Involved</h3>
-      <ul class="social-links">
-        <li><a href="https://twitter.com/natlfccoalition" class="ss-icon ss-social-circle">twitter</a></li>
-        <li><a href="https://www.facebook.com/NationalFCCoalition" class="ss-icon ss-social-circle">facebook</a></li>
-        <li><a href="https://www.youtube.com/user/globalhealthcatalyst/" class="ss-icon ss-social-circle">youtube</a></li>
-      </ul>
-      <ul class="hashtags">
-        <li>#Dance4Demand</li>
-        <li>#GFCD2014</li>
-        <li>#femalecondoms</li>
-      </ul>
-    </div>
-    
-    <div class="col-1-2 block-newsletter">
-      <?php
-      $block = block_load('block', '14');
-      print drupal_render(_block_get_renderable_array(_block_render_blocks(array($block))));
-      ?>
-    </div>
-
-<!--
-    <div class="col-1-2 block-newsletter">
-      <h3 class="title-block">Newsletter Sign Up</h3>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation aliquip consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-      <form action="https://hq-wfc2.wiredforchange.com/save" method="POST" class="form-newsletter">
-        <input type="hidden" name="organization_KEY" value="8810"/>
-        <input type="hidden" name="object" value="supporter"/>
-        <div class="form-item">
-          <label for="email">Email</label>
-          <input type="text" id="email" name="Email" />
-        </div>
-        <input type="hidden" name="link" value="groups"/>
-        <input type="hidden" name="linkKey" value="8995"/>
-        <input type="hidden" name="tag" value="newsletter"/>
-        <input type="hidden" name="redirect" value="http://gfcd.elevenelevencreative.com/thank-you.php">
-        <button type="Submit" value="Sign up" class="btn grey">Sign Up</button>
-      </form>
-    </div>
--->
+  <div class="col-1-2 block-get-involved">
+    <h3 class="block-title">Get Involved</h3>
+    <ul class="social-links">
+      <li><a href="https://twitter.com/natlfccoalition" class="ss-icon ss-social-circle">twitter</a></li>
+      <li><a href="https://www.facebook.com/NationalFCCoalition" class="ss-icon ss-social-circle">facebook</a></li>
+      <li><a href="https://www.youtube.com/user/globalhealthcatalyst/" class="ss-icon ss-social-circle">youtube</a></li>
+    </ul>
+    <ul class="hashtags">
+      <li>#Dance4Demand</li>
+      <li>#GFCD2014</li>
+      <li>#femalecondoms</li>
+    </ul>
+  </div>
+  
+  <div class="col-1-2 block-newsletter">
+    <?php
+    $block = block_load('block', '14');
+    print drupal_render(_block_get_renderable_array(_block_render_blocks(array($block))));
+    ?>
+  </div>
   
 </section> 
 
@@ -90,4 +79,3 @@
     <p>Dance with us on September 16 to demand access to female condoms. <a href="" class="btn dk-orange">Let's Dance!</a></p>
   </div>
 </section>
-<?php // print render($content); ?>
