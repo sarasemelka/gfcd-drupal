24b2e42e4b7def148707c151cc6b8c7642d61c5d
Brad Sawicki <git@bradsawicki.com>
2015-08-05 10:35:21 -0500

    Home orange section block

--- sites/all/themes/gfcd/templates/node--3.tpl.php
+++ sites/all/themes/gfcd/templates/node--3.tpl.php
@@ -17,9 +17,9 @@
 
 <section id="main" class="main">
   <div class="wrapper">
-         
+
     <div class="home-info-blocks">
-    
+
       <div class="col-1-3">
         <div class="icon"><img src="<?php print base_path() . path_to_theme(); ?>/assets/images/icon-home-01.svg" alt="Dance4Demand Icon"></div>
         <?php
@@ -43,14 +43,14 @@
         print render($block['content']);
         ?>
       </div>
-    
+
     </div>
-    
+
   </div>
 </section>
 
 <section class="bottom">
-    
+
   <div class="col-1-2 block-get-involved">
     <h3 class="block-title">Get Involved</h3>
     <ul class="social-links">
@@ -60,22 +60,25 @@
     </ul>
     <ul class="hashtags">
       <li>#Dance4Demand</li>
-      <li>#GFCD2014</li>
+      <li>#GFCD2015</li>
       <li>#femalecondoms</li>
     </ul>
   </div>
-  
+
   <div class="col-1-2 block-newsletter">
     <?php
     $block = block_load('block', '14');
     print drupal_render(_block_get_renderable_array(_block_render_blocks(array($block))));
     ?>
   </div>
-  
-</section> 
+
+</section>
 
 <section class="lets-dance">
   <div class="wrapper">
-    <p>Dance with us on September 16 to demand access to female condoms. <a href="/dance4demand" class="btn dk-orange">Let's Dance!</a></p>
+    <?php
+    $block = block_load('block', '16');
+    print drupal_render(_block_get_renderable_array(_block_render_blocks(array($block))));
+    ?>
   </div>
 </section>
