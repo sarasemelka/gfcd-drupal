b1a223ef85f9495429c318f44e080ab345780b53
Brad Sawicki <git@bradsawicki.com>
2016-09-09 18:24:54 +0200

    Fix home top js bug

--- sites/all/themes/gfcd/assets/js/scripts.js
+++ sites/all/themes/gfcd/assets/js/scripts.js
@@ -3,28 +3,28 @@
   $(function(){
 
     labelFade = function() {
-    
+
       var emailField = $('.form-newsletter #email');
       var emailLabel = $('.form-newsletter label');
-    
+
       $(emailField).bind('focus', function(){
         emailLabel.fadeOut();
       });
-  
+
       $(emailField).bind('blur', function(){
         var emailFieldVal = $(this).val();
         if(emailFieldVal == '') {
           emailLabel.fadeIn();
         }
       });
-      
+
     }
-      
-    scrollTo = function() {  
-      
+
+    scrollTo = function() {
+
       $('a[href^="#"]').bind('click', function(event) {
         var target = $( $(this).attr('href') );
-        
+
         if( target.length ) {
           event.preventDefault();
           $('html, body').animate({
@@ -32,24 +32,24 @@
           }, 500);
         }
       });
-    
+
     }
-  
+
     menuToggle = function() {
-  
+
       $('.mobile-menu-btn').click(function(){
         $('.main-nav ul, .admin-nav').toggleClass('show');
         $(this).toggleClass('active');
         return false;
       });
-  
+
     }
-  
+
   	newsletterValidation = function() {
-  	
+
     	$('.form-newsletter').submit(function(e) {
     		var formEmail = $('#email');
-    		
+
     		$(formEmail).each(function() {
   		    iValue = $(this).val();
       		if(iValue == '') {
@@ -58,7 +58,7 @@
       		}
     		});
     	});
-    	
+
     	$('.form-newsletter input[type="email"]').focus(function() {
     	   if ($(this).hasClass('error')) {
     	       $(this).keyup(function() {
@@ -69,55 +69,53 @@
     }
 
     iconZoomIn = function() {
-      
+
       $(window).scroll(function(e){
         var windowPos = $(window).scrollTop();
         var windowHeight = $(window).height();
         var homeInfoBlocks = $('.home-info-blocks');
         var homeMainPos = $(homeInfoBlocks).offset().top;
-        
+
         if(windowPos = homeMainPos) {
           e.preventDefault();
           $(homeInfoBlocks).addClass('animate');
-          console.log(homeInfoBlocks);
         }
-        
-        console.log(windowPos);
-        console.log(homeMainPos);
- 
+
       });
-  
+
     }
-    
+
     eventSlider = function() {
-      
+
       $(".event-slider").owlCarousel({
-      
+
         autoPlay: 3000,
         itemsCustom : [
         [0, 1],
         [400, 2],
         [700, 3],
-        ]        
+        ]
       });
-      
-    }
-    
-    fitVids = function() {
-      
-      $('.event-video').fitVids();
-      
+
     }
 
+    fitVids = function() {
+
+      $('.event-video').fitVids();
+
+    }
+
+    if($('.front').length) {
+      iconZoomIn();
+    }
 
     labelFade();
     scrollTo();
     menuToggle();
     newsletterValidation();
-    iconZoomIn();
     eventSlider();
     fitVids();
-    
+
   });
-  
+
 })(jQuery);
